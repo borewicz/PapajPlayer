3837abcbcff42a904ffde2d63d7c5dc5d91ccc1e
waren <waren@outlook.com>
2016-01-15 17:01:05 +0100

    init

---
+++ PapajPlayer.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package put.ai.games.papajplayer;
+
+import java.util.List;
+import java.util.Random;
+import put.ai.games.game.Board;
+import put.ai.games.game.Move;
+import put.ai.games.game.Player;
+
+public class PapajPlayer extends Player {
+
+    private Random random=new Random(0xdeadbeef);
+
+    @Override
+    public String getName() {
+        return "JP2gmd";
+    }
+
+    @Override
+    public Move nextMove(Board b) {
+        List<Move> moves = b.getMovesFor(getColor());
+        return moves.get(random.nextInt(moves.size()));
+    }
+}
